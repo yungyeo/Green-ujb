4c0c025c94ed9dff6ea80fd383f2797e9ecedfed
allis <allis@LAPTOP-H71R1AEC>
2023-09-24 14:21:29 +0900

    initial commit

---
+++ JungolHome/src/Q9006/Main.java
@@ -0,0 +1,12 @@
+package Q9006;
+
+public class Main {
+	public static void main(String[] args) {
+		int a=123;
+		float b=6.500000f;
+		
+		System.out.println("수를 출력할 때는 따옴표를 생략합니다.");
+		System.out.println(a);
+		System.out.printf("%f", b);
+	}
+}
